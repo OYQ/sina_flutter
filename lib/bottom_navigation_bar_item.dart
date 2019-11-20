aac82e1529265b8645a758ab2eb526636249117b
欧阳铨 <aquan1264@vip.qq.com>
2019-11-20 20:08:41 +0800

    添加redBean

--- lib/bottom_navigation_bar_item.dart
+++ lib/bottom_navigation_bar_item.dart
@@ -26,18 +26,57 @@ class SinaBottomNarBarItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: ()=>onTap(),
-      child: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image.asset(
-              selected ? selectedImageName : unselectedImageName, 
-              width: 40, 
-              height: 40,
+      child: Stack(
+        children: <Widget>[
+          Center(
+            child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image.asset(
+                selected ? selectedImageName : unselectedImageName, 
+                width: 40, 
+                height: 40,
+              ),
+              Text(title),
+              ],
             ),
-            Text(title),
-          ],
-        ),
+          ),
+          Positioned(
+            right: 5,
+            child: RedBean(11),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class RedBean extends StatelessWidget {
+  final double height = 25;
+  final int count;
+  RedBean(this.count);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: height,
+      padding: EdgeInsets.only(
+        left: 5,
+        right: 5
+      ),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(height/2),
+        border: Border.all(color: Colors.white, width: 2),
+        color: Color(0xFFFF0000),
+      ),
+      child: Center(
+        child: Text(
+          count.toString(),
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 15,
+          ),
+        ),  
       ),
     );
   }
